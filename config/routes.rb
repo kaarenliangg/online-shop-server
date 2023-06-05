f732cd7f7ac5ca28f9a725510aa5e417423fbe6e
Dominic <dominicwong09@gmail.com>
2023-06-06 10:58:28 +1200

    user login route & method created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :users, :only => [:index, :create]
 	resources :products, defaults: { format: :json }
+
+	post '/login' => 'users#login'
 end
