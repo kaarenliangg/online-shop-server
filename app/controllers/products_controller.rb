9a07f362f1378c87f834664df9b80252d565cc3c
Stuart Cannone <stuartcannone@gmail.com>
2023-06-05 15:09:27 +1000

    added products and cloudinary

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,60 @@
+class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+
+        render json: @products
+    end
+
+    def new
+        @product = Product.new
+
+        render json: @product
+    end
+
+    def update
+        product = Product.find params[:id]
+        if params[:file].present?
+        req = Cloudinary::Uploader.upload(params[:file])
+        product.image = req["public_id"]
+        
+        end
+        product.update_attributes product_params
+        product.save
+
+        render json: product, :status => :ok
+    end
+
+    def create
+        product = Product.create product_params
+        if params[:file].present?
+        req = Cloudinary::Uploader.upload(params[:file])
+        product.image = req["public_id"]
+        product.save
+        end
+
+        render json: @product, :status => :created
+    end
+
+    def edit
+        @product = Product.find params[:id]
+
+        render json: @product, :status => :ok
+    end
+    
+    def show
+        @product = Product.find params[:id]
+
+        render json: @product
+    end
+
+    def destroy
+        product = Product.find params[:id]
+        product.destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:product).permit(:product_code, :product_name, :product_categories, :product_description , :image, :costprice, :retailprice )
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_05_031908) do
+ActiveRecord::Schema.define(version: 2023_06_05_040423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.text "product_code"
+    t.text "product_name"
+    t.text "product_categories"
+    t.text "product_description"
+    t.text "image"
+    t.decimal "costprice", precision: 10, scale: 2
+    t.decimal "retailprice", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fullname"
