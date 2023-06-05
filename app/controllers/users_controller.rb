ddaababf4fd9a7d161cebbc5bd4ed89c78087c5d
Dominic <dominicwong09@gmail.com>
2023-06-05 15:32:57 +1200

    incl routes. fixed user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password, :address, :admin, :contact_number)
+        params.require(:user).permit(:fullname, :email, :password, :address, :admin, :contact_number)
     end
 end
