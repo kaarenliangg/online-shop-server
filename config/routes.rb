4b425ac49515229e498869e3cef6a6244b548cf4
Stuart Cannone <stuartcannone@gmail.com>
2023-06-07 12:52:02 +1000

    added orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources :users, :only => [:index, :create]
 	resources :products, defaults: { format: :json }
-
+	resources :orders, defaults: { format: :json }
+	
 	post '/login' => 'users#login'
 	get '/profile' => 'users#profile'
 end
