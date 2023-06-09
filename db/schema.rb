82bf4ba931bc2ff2a85b6a2ee478b4d3895a8989
kaarenliangg <karenliang9546@gmail.com>
2023-06-09 10:40:18 +1000

    schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_016853) do
+ActiveRecord::Schema.define(version: 2023_06_09_003510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
     t.text "orderstatus"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "orders_products", id: false, force: :cascade do |t|
