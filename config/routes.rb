9051093a3190bf249e718e82d040cd7ef69089b0
Dominic <dominicwong09@gmail.com>
2023-06-09 19:08:19 +1200

    Forgot PW - ActionMailer Setup

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
 	
 	post '/login' => 'users#login'
 	get '/profile' => 'users#profile'
+	post '/password/forgot' => 'passwords#forgot'
+	post 'password/reset' => 'passwords#reset'
+	
 end
