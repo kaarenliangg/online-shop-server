643e0d3af1680b10d6d121e10c35a80f1b7992ee
Dominic <dominicwong09@gmail.com>
2023-06-10 15:34:03 +1200

    linked checkout page to db products

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -6,53 +6,58 @@ class CheckoutsController < ApplicationController
         prod_url = ""
 
         session = Stripe::Checkout::Session.create({
-          shipping_address_collection: {allowed_countries: ['AU']},
-          shipping_options: [
-            {
-              shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                  amount: 0,
-                  currency: 'aud',
-                },
-                display_name: 'Free shipping',
-                delivery_estimate: {
-                  minimum: {
-                    unit: 'business_day',
-                    value: 5,
-                  },
-                  maximum: {
-                    unit: 'business_day',
-                    value: 7,
-                  },
-                },
-              },
-            },
-            {
-              shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                  amount: 1500,
-                  currency: 'aud',
-                },
-                display_name: 'Next day air',
-                delivery_estimate: {
-                  minimum: {
-                    unit: 'business_day',
-                    value: 1,
-                  },
-                  maximum: {
-                    unit: 'business_day',
-                    value: 1,
-                  },
-                },
-              },
-            },
-          ],
+			shipping_address_collection: {allowed_countries: ['AU']},
+			shipping_options: [
+            	{
+              		shipping_rate_data: {
+                		type: 'fixed_amount',
+                		fixed_amount: {
+                  			amount: 0,
+                  			currency: 'aud',
+                		},
+                		display_name: 'Free shipping',
+                		delivery_estimate: {
+                  			minimum: {
+                    			unit: 'business_day',
+                    			value: 5,
+                  			},
+                  			maximum: {
+                    			unit: 'business_day',
+                    			value: 7,
+                  			},
+                		},
+              		},
+            	},
+				{
+					shipping_rate_data: {
+						type: 'fixed_amount',
+						fixed_amount: {
+							amount: 1500,
+							currency: 'aud',
+						},
+						display_name: 'Next day air',
+						delivery_estimate: {
+							minimum: {
+								unit: 'business_day',
+								value: 1,
+							},
+							maximum: {
+								unit: 'business_day',
+								value: 1,
+							},
+						},
+					},
+				},
+			],
             line_items: [{
-              # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
-              price: 'price_1NGfozHK0jF5AwAI1WmlZOIz',
-              quantity: 1,
+				price_data: {
+					currency: 'aud',
+					product_data: {
+						name: Product.first.product_name
+					},
+					unit_amount: (Product.first.retail_price * 100).to_i
+              	},
+				quantity: 1
             }],
             mode: 'payment',
             success_url: dev_url + '?success=true',
